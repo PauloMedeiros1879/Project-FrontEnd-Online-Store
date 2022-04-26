66f5dfe042c5cb10b49ad3727d59ea80c68363f4
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-04-26 18:45:50 -0300

    C04 - Feito requisito 4. Em Pair Programming

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const categoriesObj = await getCategories();
+    this.setState({
+      categories: categoriesObj,
+    });
+  };
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <label htmlFor="seach" data-testid="home-initial-message">
@@ -12,6 +32,13 @@ class Home extends Component {
         <Link to="/shopping-cart" data-testid="shopping-cart-button">
           Carrinho De Compras
         </Link>
+        <ul className="categories">
+          {categories.map(({ name }, i) => (
+            <li key={ i } data-testid="category">
+              {name}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ export async function getCategories() {
   try {
     const api = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
     const categories = await api.json();
+    console.log(categories);
     return categories;
   } catch (error) {
     console.log(error);
