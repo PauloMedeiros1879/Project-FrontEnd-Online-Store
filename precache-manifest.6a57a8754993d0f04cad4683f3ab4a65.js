2d79681d34f1a4d6a559ba245509dd2995eef06c
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-09-02 09:41:00 -0400

    Updates

---
+++ precache-manifest.6a57a8754993d0f04cad4683f3ab4a65.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59da919a31672a663794dd3ea04fba86",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "fe7f1dd732ea2621a17b",
+    "url": "/app-toot-name/static/css/main.5cc0da02.chunk.css"
+  },
+  {
+    "revision": "beb1077db005dd91d681",
+    "url": "/app-toot-name/static/js/2.16a647cf.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/app-toot-name/static/js/2.16a647cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7f1dd732ea2621a17b",
+    "url": "/app-toot-name/static/js/main.4e225b45.chunk.js"
+  },
+  {
+    "revision": "1b1943e62e59b0082ef1",
+    "url": "/app-toot-name/static/js/runtime-main.946983d5.js"
+  },
+  {
+    "revision": "18cdcc1e32eae230436fb545ac656435",
+    "url": "/app-toot-name/static/media/bank-card.18cdcc1e.svg"
+  },
+  {
+    "revision": "2dbea7bb09890b662788ee0f6d2637d1",
+    "url": "/app-toot-name/static/media/cart.2dbea7bb.svg"
+  },
+  {
+    "revision": "cdedd077e1facaa51852e8e82075a465",
+    "url": "/app-toot-name/static/media/empty-cart.cdedd077.svg"
+  },
+  {
+    "revision": "5db86616beaaac3383a22ae895b1ddb2",
+    "url": "/app-toot-name/static/media/home.5db86616.svg"
+  },
+  {
+    "revision": "52310e5170d42b8a65ae32a6537015fa",
+    "url": "/app-toot-name/static/media/mastercard.52310e51.svg"
+  },
+  {
+    "revision": "baa95d583f9e2f1d0d49405624aea990",
+    "url": "/app-toot-name/static/media/star.baa95d58.svg"
+  },
+  {
+    "revision": "859d7240a108411880f46b4233f18259",
+    "url": "/app-toot-name/static/media/star_colored.859d7240.svg"
+  },
+  {
+    "revision": "87ded0e75ad7c5da3b021fe2f4af76c0",
+    "url": "/app-toot-name/static/media/visa.87ded0e7.svg"
+  }
+]);
